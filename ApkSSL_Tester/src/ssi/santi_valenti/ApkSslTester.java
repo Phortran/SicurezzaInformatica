3f28e22b1888110ffb7aa4ea9d2077002dcd4407
Phortran <neonoetica@gmail.com>
2015-05-25 17:23:06 +0200

    scheletro base

---
+++ ApkSSL_Tester/src/ssi/santi_valenti/ApkSslTester.java
@@ -0,0 +1,13 @@
+package ssi.santi_valenti;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApkSslTester {
+	private static final Logger log = LoggerFactory.getLogger(ApkSslTester.class);
+
+	public static void main(String[] args) {
+		log.error("valenti sbaglia");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # SicurezzaInformatica
 Progetto di sicurezza informatica e di internet, AA 14-15
-Autori: Gabriele Santi, Alessandro Valenti
+
+#Autori
+Gabriele Santi, Alessandro Valenti
 
 # Branch
-
 Branch di Gabriele
